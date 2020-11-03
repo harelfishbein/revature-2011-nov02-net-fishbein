e7fbc8598d6a130f94a31be165afaf2c49af062d
harelfishbein <hmfishbein@gmail.com>
2020-11-03 12:02:11 -0500

    test commit for username setting on machine predator

--- first-dotnet-project/Program.cs
+++ first-dotnet-project/Program.cs
@@ -1,11 +1,8 @@
 ﻿using System;
 
-namespace first_dotnet_project
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+namespace first_dotnet_project {
+    class Program {
+        static void Main(string[] args) {
             Console.WriteLine("Hello World!");
         }
     }
